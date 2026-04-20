1ad8f80128b1a7edca463e419ca571ecc6c1a8cb
Goal651 <bugiriwilson651@gmail.com>
2026-04-20 07:58:44 +0200

    removing unused files

--- lib/services/fieldService.ts
+++
@@ -1,3 +0,0 @@
-// Field service removed as part of Dreamize rebrand cleanup.
-// Fields and Companies have been removed from the system.
-export {};
--- lib/services/index.ts
+++
@@ -1,5 +0,0 @@
-// Service layer exports for Dreamize LMS
-
-export { UserService } from './userService';
-export { SessionService } from './sessionService';
-export { PaymentService } from './paymentService';
