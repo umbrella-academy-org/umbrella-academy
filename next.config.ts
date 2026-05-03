637ef6a6f429acb133465b72ecd3ca6a9187aeec
Goal651 <bugiriwilson651@gmail.com>
2026-05-03 10:21:17 +0200

    feat: add support for images from Unsplash in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         hostname: "cdn.sanity.io",
         pathname: "/images/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
